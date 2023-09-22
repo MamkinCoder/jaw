dac9f4d4e462132ebdcb782291c46ac28bcf0552
Yaroslav Balahonov <yabalahonov@yandex.ru>
2023-09-22 19:17:24 +0600

    stderror get_data.php

--- php/get_data.php
+++ php/get_data.php
@@ -4,6 +4,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$process->stdout->on('data', function ($chunk) use (&$data) {
+    $data .= $chunk;
+});
+
+$process->stderr->on('data', function ($chunk) {
+    echo 'Error in child process: ' . $chunk . PHP_EOL;
+});
+
 require '../vendor/autoload.php';
 
 use React\EventLoop\Factory;
